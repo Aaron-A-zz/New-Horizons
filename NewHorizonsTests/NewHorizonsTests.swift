4095f64f252e5b56080cd4c1a563960a585a5f79
Aaron A <Aaron@Mav3r1ck.io>
2015-10-18 06:32:50 -0400

    Initial Commit

---
+++ NewHorizonsTests/NewHorizonsTests.swift
@@ -0,0 +1,36 @@
+//
+//  NewHorizonsTests.swift
+//  NewHorizonsTests
+//
+//  Created by Aaron Ackerman on 10/18/15.
+//  Copyright © 2015 Mav3r1ck. All rights reserved.
+//
+
+import XCTest
+@testable import NewHorizons
+
+class NewHorizonsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
