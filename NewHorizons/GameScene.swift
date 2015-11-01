f067a6153dea4cac4f712ab039a79a3aa297c1a3
Aaron A <Aaron@Mav3r1ck.io>
2015-11-01 14:41:29 -0500

    SpriteKit with Swift III

--- NewHorizons/GameScene.swift
+++ NewHorizons/GameScene.swift
@@ -8,10 +8,13 @@
 
 import SpriteKit
 
+
+import SpriteKit
+
 class GameScene: SKScene {
     
-    var satellite = SKSpriteNode(imageNamed: "Sat2")
-    var asteroid = SKSpriteNode(imageNamed: "Asteroid")
+    let satellite = SKSpriteNode(imageNamed: "Sat2")
+    
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -19,23 +22,37 @@ class GameScene: SKScene {
         satellite.position = CGPoint(x:frame.size.width / 2, y: frame.size.height / 2)
         addChild(satellite)
         
-        asteroid.position = CGPoint(x:frame.size.width / 2, y: frame.size.height / 1.3)
-        addChild(asteroid)
+        let spawnRandomAsteroid = SKAction.runBlock(spawnAsteroid)
+        let waitTime = SKAction.waitForDuration(1.0)
+        let sequence = SKAction.sequence([spawnRandomAsteroid,waitTime])
+        runAction(SKAction.repeatActionForever(sequence))
+        
+        physicsWorld.gravity = CGVectorMake(0.0, -0.9)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first! as UITouch
         let touchLocation = touch.locationInNode(self)
         print(touchLocation)
-        //satellite.position = touchLocation
         
         let moveTo = SKAction.moveTo(touchLocation, duration: 1.0)
         satellite.runAction(moveTo)
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-
+        
     }
     
+    
+    func randomNumber(min min: CGFloat, max: CGFloat) -> CGFloat {
+        let random = CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+        return random * (max - min) + min
+    }
+    
+    func spawnAsteroid() {
+        
+        let asteroid = SKSpriteNode(imageNamed: "Asteroid")
+        asteroid.position = CGPoint(x: frame.size.width * randomNumber(min: 0, max: 1), y: frame.size.height + asteroid.size.height)
+        asteroid.physicsBody = SKPhysicsBody(circleOfRadius: asteroid.frame.size.width * 0.3)
+        addChild(asteroid)
+        
+    }
+
 }
