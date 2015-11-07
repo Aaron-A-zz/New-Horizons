aee243c794d7fa5b8892825c9529ca1b7b1eafed
Aaron <July 25 1983>
2015-11-07 11:36:28 -0500

    SpriteKit with Swift IV

--- NewHorizons/GameScene.swift
+++ NewHorizons/GameScene.swift
@@ -8,8 +8,13 @@
 
 import SpriteKit
 
-
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    enum bitMask: UInt32 {
+        case satellite = 1
+        case asteroid = 2
+        case frame = 4
+    }
     
     let satellite = SKSpriteNode(imageNamed: "Sat2")
     
@@ -17,6 +22,20 @@ class GameScene: SKScene {
         /* Setup your scene here */
         backgroundColor = UIColor.blackColor()
         satellite.position = CGPoint(x:frame.size.width / 2, y: frame.size.height / 2)
+        //A
+        satellite.physicsBody = SKPhysicsBody(texture: satellite.texture!, size: satellite.frame.size)
+        //B
+        satellite.physicsBody?.dynamic = false
+        //C
+        satellite.physicsBody?.affectedByGravity = false
+        //D
+        satellite.physicsBody?.allowsRotation = false
+        //E
+        satellite.physicsBody?.categoryBitMask = bitMask.satellite.rawValue
+        //F
+        satellite.physicsBody?.contactTestBitMask = bitMask.asteroid.rawValue
+        //G
+        satellite.physicsBody?.collisionBitMask = 0
         addChild(satellite)
         
         let spawnRandomAsteroid = SKAction.runBlock(spawnAsteroid)
@@ -24,17 +43,21 @@ class GameScene: SKScene {
         let sequence = SKAction.sequence([spawnRandomAsteroid,waitTime])
         runAction(SKAction.repeatActionForever(sequence))
         
+        physicsWorld.contactDelegate = self
         physicsWorld.gravity = CGVectorMake(0.0, -0.9)
+        
+        satellite.physicsBody?.contactTestBitMask = bitMask.asteroid.rawValue
+        satellite.physicsBody?.collisionBitMask = bitMask.frame.rawValue
+        
     }
     
+  
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first! as UITouch
         let touchLocation = touch.locationInNode(self)
-        print(touchLocation)
-        
+        //print(touchLocation)
         let moveTo = SKAction.moveTo(touchLocation, duration: 1.0)
         satellite.runAction(moveTo)
-        
     }
     
     
@@ -43,13 +66,31 @@ class GameScene: SKScene {
         return random * (max - min) + min
     }
     
+    
     func spawnAsteroid() {
-        
         let asteroid = SKSpriteNode(imageNamed: "Asteroid")
         asteroid.position = CGPoint(x: frame.size.width * randomNumber(min: 0, max: 1), y: frame.size.height + asteroid.size.height)
-        asteroid.physicsBody = SKPhysicsBody(circleOfRadius: asteroid.frame.size.width * 0.3)
+        asteroid.physicsBody = SKPhysicsBody(texture: asteroid.texture!, size: asteroid.frame.size)
+        asteroid.physicsBody?.categoryBitMask = bitMask.asteroid.rawValue
+        asteroid.physicsBody?.contactTestBitMask = bitMask.satellite.rawValue
         addChild(asteroid)
         
     }
-
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        let contactMask = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        
+        switch(contactMask) {
+        case bitMask.satellite.rawValue | bitMask.asteroid.rawValue:
+            let secondNode = contact.bodyB.node
+            secondNode?.physicsBody?.allowsRotation = true
+            let firstNode = contact.bodyA.node
+            firstNode?.physicsBody?.allowsRotation = true
+            firstNode?.removeFromParent()
+            
+        default:
+            return
+        }
+    }
+    
 }
